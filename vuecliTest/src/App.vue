681cfaffc1d4346f3d4965a30e6ad762b4fb026b
yellowei <hw0521@vip.qq.com>
2018-04-20 19:22:35 +0800

    Second Day Demo

--- vuecliTest/src/App.vue
+++ vuecliTest/src/App.vue
@@ -1,19 +1,26 @@
 <template>
   <div id="app">
     <img src="./assets/logo.png">
+    <div>
+      <router-link to="/">首页</router-link> |
+      <router-link to="/hi">Hi Page</router-link> |
+      <router-link :to="{name:'hi1', params:{name:'yellowei', id:'001'}}">-Hi1 Page</router-link> |
+      <router-link to="/hi/hi2">-Hi2 Page</router-link> 
+    </div>
+    <!-- <p> {{ $route.name }} </p> -->
     <router-view/>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'App'
-}
+  name: "App"
+};
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
